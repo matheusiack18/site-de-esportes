23bc38409731424fe218b59b2a59788a43742982
Daniel Mello <danielmello.nfa@gmail.com>
2023-02-08 05:49:56 -0300

    commit de estilização quase finalzizada

--- lojaproject/lojaapp/forms.py
+++ lojaproject/lojaapp/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from.models import Pedido_order, Cliente
 from django.contrib.auth.models import User
+from django import forms
+from django.core.mail import send_mail
+from lojaproject import settings
 # from setings impot EMAIL_FALE_CONOSCO
 
 class Checar_PedidoForm(forms.ModelForm):
@@ -27,12 +30,20 @@ class ClienteEntrarForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput())
     password = forms.CharField(widget=forms.PasswordInput())
 
+# class FaleConoscoForm(forms.Form):
+#     Nome = forms.CharField(label = "Digite seu Nome")
+#     email = forms.EmailField(label = "Digite seu email")
+#     mensagem = forms.CharField(required=True, widget=forms.Textarea)
+
+
 class FaleConoscoForm(forms.Form):
-    Nome = forms.CharField(label = "Digite seu Nome")
-    email = forms.EmailField(label = "Digite seu email")
-    mensagem = forms.CharField(required=True, widget=forms.Textarea)
-
-    def enviar_mensagem_por_email():
-
-        EMAIL_FALE_CONOSCO
-        return
+    nome = forms.CharField(required=True, label='Informe seu Nome')
+    email_origem = forms.EmailField(label ='Informe seu email')
+    mensagem = forms.CharField(required=True, widget=forms.Textarea(attrs={'rows':5}))
+    
+    def enviar_mensagem_por_email(self):
+        send_mail('FALE CONOSCO: Mensagem recebida.',
+        self.data['mensagem'],
+        self.data['email_origem'],
+        ['settings.EMAIL_FALE_CONOSCO'],
+        fail_silently=False)
